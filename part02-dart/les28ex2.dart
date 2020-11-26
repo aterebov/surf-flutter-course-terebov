d2fccfa8180b1102f7ef291a9fb05d209cbc949d
Andrey Terebov <aterebovdev@gmail.com>
2020-11-27 00:29:41 +0300

    Create les28ex2.dart

---
+++ part02-dart/les28ex2.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> stringList = [];
+  stringList.add("s1");
+  // stringList.add(12);
+  // Error: The argument type 'int' can't be assigned to the parameter type 'String'
+}
