bd5f6a54749b00fe346e32048906ff60376dabac
Andrey Terebov <aterebovdev@gmail.com>
2020-12-16 08:36:58 +0300

    Add AppBar with Text

--- places/lib/ui/screen/sight_list_screen.dart
+++ places/lib/ui/screen/sight_list_screen.dart
@@ -8,6 +8,20 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Список\nинтересных мест',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 32,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        toolbarHeight: 72,
+      ),
+    );
   }
 }
