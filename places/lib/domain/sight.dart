1b6d5760c766ffa4915c7a914a8ad1da0f05fa42
Andrey Terebov <aterebovdev@gmail.com>
2020-12-17 23:58:56 +0300

    Add Sight model and mocks

---
+++ places/lib/domain/sight.dart
@@ -0,0 +1,11 @@
+// Sight - модель данных
+class Sight {
+  String name;
+  double lat; // широта
+  double lon; // долгота
+  String url;
+  String detail;
+  String type;
+
+  Sight(this.name, this.lat, this.lon, this.url, this.detail, this.type);
+}
